66e5300c2bac556e73a288ee933c73e09b8c7c09
CintiaNataly <164966730+CintiaNataly@users.noreply.github.com>
2024-08-24 22:29:21 -0300

    Desafio semanal 2

---
+++ Ejercicio 1/README.md
@@ -0,0 +1,3 @@
+# Ejercicio 1
+
+Tiene una función que verifique si un número es primo.
---
+++ Ejercicio 1/main.mjs
@@ -0,0 +1,7 @@
+//require
+
+//const numPrimo = require("./index.cjs");
+
+import numPrimo from "./index.cjs";
+
+console.log(numPrimo(5));
